f02cc441a0759c1adce61a7df338ac586431fa99
0sha-dow0 <shaikhshadab.hossain@gmail.com>
2023-02-27 16:52:29 +0530

    added new feature in yet to be developed html page

--- script.js
+++ script.js
@@ -9,6 +9,37 @@ document.body.onpointermove = event =>{
 const random =()=>{
 
 }
+////////////////////////////////////////////////////////////////////////////////////
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+let interval = null;
+document.addEventListener('DOMContentLoaded',()=>{
+        text2=document.getElementById('yet') 
+            let iteration = 0;
+                        
+            clearInterval(interval);
+            
+            interval = setInterval(() => {
+                text2.innerText = text2.innerText
+                .split("")
+                .map((letter, index) => {
+                  if(index < iteration) {
+                    return text2.dataset.value[index];
+                  }
+                
+                  return letters[Math.floor(Math.random() * 26)]
+                })
+                .join("");
+            
+              if(iteration >= text2.dataset.value.length){ 
+                clearInterval(interval);
+              }
+            
+              iteration += 1/3;
+            }, 30)           
+})
+
 
 
 
