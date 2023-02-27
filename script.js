9de6ccbc97e82d5e5efcad1043b73c681787b4f4
0sha-dow0 <shaikhshadab.hossain@gmail.com>
2023-02-27 17:40:16 +0530

    hacker feeels feature added in the home page

--- script.js
+++ script.js
@@ -40,6 +40,35 @@ document.addEventListener('DOMContentLoaded',()=>{
             }, 30)           
 })
 
-
+const myinterval =setInterval(myGreeting,1000)
+function myGreeting() {
+  let variable = ['html','css','js','python','react']
+    number = Math.floor(Math.random()*10/2)
+    console.log(variable[number])
+    text = document.getElementById(variable[number])
+      let iteration = 0;
+      
+      clearInterval(interval);
+      
+      interval = setInterval(() => {
+        text.innerText = text.innerText
+          .split("")
+          .map((letter, index) => {
+            if(index < iteration) {
+              return text.dataset.value[index];
+            }
+          
+            return letters[Math.floor(Math.random() * 26)]
+          })
+          .join("");
+        
+        if(iteration >= text.dataset.value.length){ 
+          clearInterval(interval);
+        }
+        
+        iteration += 1 / 3;
+      }, 30);
+  
+}
 
 
